cca0ab42004a54e37939ba76abec3ddf49a78303
Arthur XU <arthurk.dev@gmail.com>
2015-10-21 17:03:00 +0800

    Made the result of regex match to be better

--- Pod/Regex.swift
+++ Pod/Regex.swift
@@ -8,7 +8,26 @@
 
 import Foundation
 
+public struct RegexResult {
+    
+    public let text: String
+    public var matches = [(content:String, range: NSRange, type: NSTextCheckingType)]()
+    
+    public init(_ text: String) {
+        self.text = text
+    }
+    
+    public var hasMatch: Bool {
+        return self.matches.count>0
+    }
+    public var isCompleteMatch: Bool {
+        guard self.matches.count==1 else {return false}
+        return self.matches[0].content.characters.count==self.text.characters.count
+    }
+}
+
 public struct Regex {
+    
     public var regex: NSRegularExpression?
     
     public init(_ pattern: String, options: NSRegularExpressionOptions=[]) {
@@ -19,34 +38,43 @@ public struct Regex {
         }
     }
     
-    public func match(text: String) -> Bool {
-        guard let regex = self.regex else {return false}
+    public func match(text: String) -> RegexResult {
+        var result = RegexResult(text)
+        guard let regex = self.regex else {return result}
         let matches = regex.matchesInString(text, options: .ReportCompletion, range: NSMakeRange(0, text.characters.count))
-        return matches.count > 0
+        for matchResult in matches {
+            let content = (text as NSString).substringWithRange(matchResult.range)
+            result.matches.append((content, matchResult.range, matchResult.resultType))
+        }
+        return result
     }
 }
 
-public func regexMatch(text: String, pattern: String, options: NSRegularExpressionOptions=[]) -> Bool {
+public func regexMatch(text: String, pattern: String, options: NSRegularExpressionOptions=[]) -> RegexResult {
+    var result = RegexResult(text)
     do{
         let regex = try NSRegularExpression(pattern: pattern, options:options)
-        let matches = regex.matchesInString(text, options: .ReportCompletion, range: NSMakeRange(0, text.characters.count))
-        return matches.count > 0
+        let matches = regex.matchesInString(text, options:.ReportCompletion, range: NSMakeRange(0, text.characters.count))
+        for matchResult in matches {
+            let content = (text as NSString).substringWithRange(matchResult.range)
+            result.matches.append((content, matchResult.range, matchResult.resultType))
+        }
     } catch {
         print("Fail to init Regex with pattern[\(pattern)]")
     }
-    return false
+    return result
 }
 
 infix operator =~ {
 associativity none
 precedence 130
 }
-public func =~ (lhs: String, rhs: String) -> Bool {
+public func =~ (lhs: String, rhs: String) -> RegexResult {
     return regexMatch(lhs, pattern: rhs)
 }
 
 extension String {
-    public func regex(pattern: String) -> Bool {
+    public func regex(pattern: String) -> RegexResult {
         return regexMatch(self, pattern: pattern)
     }
 }
